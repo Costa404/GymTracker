169fccfe83f8d95c5c9b65ab3b1bac07e7ebe4ff
costa404 <nuno-costa17@hotmail.com>
2026-04-16 14:48:06 +0100

    Docs: App Update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,8 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0,">
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
